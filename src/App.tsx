3ceb519ee7671544212ad64526e60e7adee431f0
Vivek Gupta <vivekgupta86988@gmail.com>
2023-11-05 15:26:05 +0530

    SOLID principles in React.js

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import { DIP } from "./principles/DIP";
+import { LSP } from "./principles/LSP";
+import { OCP } from "./principles/OCP";
+import { SRP } from "./principles/SRP";
+
+function App() {
+  return (
+    <div className="flex min-w-full h-full justify-center items-center p-8">
+      {/* <SRP /> */}
+      {/* <OCP /> */}
+      {/* <LSP /> */}
+      <DIP />
+    </div>
+  );
+}
+
+export default App;
